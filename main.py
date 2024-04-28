952db571a72a667fabe8f76a87aa03e5da241ed6
MaciejCiesielsk <maciej182.mc@gmail.com>
2024-04-28 17:53:31 +0200

    dodaje gowno

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ parser=argparse.ArgumentParser()
 parser.add_argument("-generate", action="store_true" , help="Generate random graph")
 parser.add_argument("-user-provided", action="store_true" , help="Generate graph from user input")
 
+japierdole co sie dzieje i dlacego tego nie widac z mainie
