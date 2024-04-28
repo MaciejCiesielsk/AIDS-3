08a8ecb31af9dd31a7353fb6f7bff82d82bf8c51
MaciejCiesielsk <maciej182.mc@gmail.com>
2024-04-28 17:43:42 +0200

    Na dobry poczatek

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+import os 
+import argparse 
+
+parser=argparse.ArgumentParser()
+parser.add_argument("-generate", action="store_true" , help="Generate random graph")
+parser.add_argument("-user-provided", action="store_true" , help="Generate graph from user input")
+
