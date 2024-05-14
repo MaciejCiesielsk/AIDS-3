03e142a975b4c14da62a9505f3a52672ad62151d
MaciejCiesielsk <maciej182.mc@gmail.com>
2024-05-14 11:55:49 +0200

    dodaje table dziala i wszystko

--- table.py
+++
@@ -1,6 +0,0 @@
-class Table():
-    def generate_graph(self, sauration, nodes):
-        if saturation > 1 and sauration < 0:
-            print("Saturation must be a value between [0, 1].")
-            return None
-        
